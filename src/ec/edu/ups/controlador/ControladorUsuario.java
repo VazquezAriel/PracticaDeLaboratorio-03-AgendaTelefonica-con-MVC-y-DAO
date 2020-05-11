fbefce4bd68d15c087dc775ad95c78ae11a4f7ac
ariel <ariel@LAPTOP-MQ6B7FKQ.local>
2020-05-11 16:24:40 -0500

    Implementacion de los metodos CURD en la clase UsuarioDAO y Creacion de los metodos registrar(), verUsuario(), actualizar(), eliminar(), verUsuarios() dentro de la clase ControladorUsuario

--- src/ec/edu/ups/controlador/ControladorUsuario.java
+++ src/ec/edu/ups/controlador/ControladorUsuario.java
@@ -5,9 +5,11 @@
  */
 package ec.edu.ups.controlador;
 
+import ec.edu.ups.dao.UsuarioDAO;
 import ec.edu.ups.idao.IUsuarioDAO;
 import ec.edu.ups.modelo.Usuario;
 import ec.edu.ups.vista.VistaUsuario;
+import java.util.List;
 
 /**
  *
@@ -18,5 +20,41 @@ public class ControladorUsuario {
     private VistaUsuario vistaUsuario;
     private Usuario usuario;
     private IUsuarioDAO usuarioDAO;
+
+    public ControladorUsuario(VistaUsuario vistaUsuario) {
+        this.vistaUsuario = vistaUsuario;
+        this.usuarioDAO = new UsuarioDAO();
+    }
     
+    public void registrar() {
+        usuario = vistaUsuario.ingresarUsuario();
+        usuarioDAO.create(usuario);
+    }
+
+    //llama al DAO para obtener un cliente por el id y luego los muestra en la vista
+    public void verUsuario() {
+        String cedula = vistaUsuario.buscarUsuario();
+        usuario = usuarioDAO.read(cedula);
+        vistaUsuario.verUsuario(usuario);
+    }
+
+    //llama al DAO para actualizar un cliente
+    public void actualizar() {
+        usuario = vistaUsuario.actualizarUsuario();
+        usuarioDAO.update(usuario);
+    }
+
+    //llama al DAO para eliminar un cliente
+    public void eliminar() {
+        usuario = vistaUsuario.eliminarUsuario();
+        usuarioDAO.delete(usuario);
+    }
+
+    //llama al DAO para obtener todos los clientes y luego los muestra en la vista
+    public void verUsuarios() {
+        List<Usuario> usuarios;
+        usuarios = usuarioDAO.findAll();
+        vistaUsuario.verUsuarios(usuarios);
+    }
+
 }
